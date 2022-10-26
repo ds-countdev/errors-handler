81584f279f091fca25b5add5e36a2f2448e34997
diego alejandro <diegoalejandro.solano@hotmail.com>
2022-10-25 21:43:48 -0500

    creacion del proyecto

---
+++ src/main/java/mx/com/errors/handler/ErrorsHandlerApplication.java
@@ -0,0 +1,13 @@
+package mx.com.errors.handler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrorsHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErrorsHandlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/com/errors/handler/ErrorsHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.errors.handler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErrorsHandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
